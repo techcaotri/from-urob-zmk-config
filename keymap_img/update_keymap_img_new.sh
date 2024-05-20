f22581d61ced16b82cb98f4ec1a89c957a97b373
Tri Pham <techcaotri@gmail.com>
2024-05-20 11:22:11 +0700

    Finish updating keymap image for Corne choc

--- keymap_img/update_keymap_img_new.sh
+++ keymap_img/update_keymap_img_new.sh
@@ -2,12 +2,12 @@
 
 echo 'Parsing ZMK keymap...'
 # keymap -c adv360pro_keymap_config.yaml parse -c 10 -z ../config/adv360pro.keymap --base-keymap adv360pro_keymap_base.yaml > adv360pro_keymap.yaml
-keymap -c adv360pro_keymap_config.yaml parse -c 10 -z ../config/adv360pro.keymap > adv360pro_keymap.yaml
+keymap -c corne_keymap_config.yaml parse -c 10 -z ../config/corne.keymap > corne_keymap.yaml
 
 echo 'Adjusting keymap yaml...'
-./keymap_img_adjuster.py adv360pro_keymap.yaml
+./keymap_img_adjuster.py corne_keymap.yaml
 
 echo 'Drawing keymap...'
-keymap -c adv360pro_keymap_config.yaml draw --select-layers base nav fn num sys mouse  -k adv360pro_urob adv360pro_keymap.yaml > adv360pro_keymap.svg
-keymap -c adv360pro_keymap_config.yaml draw --keys-only --select-layers base nav fn num sys mouse  -k adv360pro_urob adv360pro_keymap.yaml > adv360pro_keymap_keys.svg
-keymap -c adv360pro_keymap_config.yaml draw --combos-only --select-layers base nav fn num sys mouse  -k adv360pro_urob adv360pro_keymap.yaml > adv360pro_keymap_combos.svg
+keymap -c corne_keymap_config.yaml draw --select-layers base nav fn num sys mouse  -k corne_rotated corne_keymap.yaml > corne_keymap.svg
+keymap -c corne_keymap_config.yaml draw --keys-only --select-layers base nav fn num sys mouse  -k corne_rotated corne_keymap.yaml > corne_keymap_keys.svg
+keymap -c corne_keymap_config.yaml draw --combos-only --select-layers base nav fn num sys mouse  -k corne_rotated corne_keymap.yaml > corne_keymap_combos.svg
