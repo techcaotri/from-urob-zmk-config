67c9d832638e64744a79f8ab2afbbc64af049cfc
Tri Pham <techcaotri@gmail.com>
2025-04-02 11:26:53 +0700

    Initial update to Eyelash Corne branch

--- keymap_img/update_keymap_img_new.sh
+++ keymap_img/update_keymap_img_new.sh
@@ -2,12 +2,9 @@
 
 echo 'Parsing ZMK keymap...'
 # keymap -c adv360pro_keymap_config.yaml parse -c 10 -z ../config/adv360pro.keymap --base-keymap adv360pro_keymap_base.yaml > adv360pro_keymap.yaml
-keymap -c corne_keymap_config.yaml parse -c 10 -z ../config/corne.keymap > corne_keymap.yaml
-
-echo 'Adjusting keymap yaml...'
-./keymap_img_adjuster.py corne_keymap.yaml
+keymap -c ../keymap_drawer.config.yaml parse -z ../config/eyelash_corne.keymap > eyelash_corne_keymap.yaml
 
 echo 'Drawing keymap...'
-keymap -c corne_keymap_config.yaml draw --select-layers base nav fn num sys mouse  -k corne_rotated corne_keymap.yaml > corne_keymap.svg
-keymap -c corne_keymap_config.yaml draw --keys-only --select-layers base nav fn num sys mouse  -k corne_rotated corne_keymap.yaml > corne_keymap_keys.svg
-keymap -c corne_keymap_config.yaml draw --combos-only --select-layers base nav fn num sys mouse  -k corne_rotated corne_keymap.yaml > corne_keymap_combos.svg
+keymap -c ../keymap_drawer.config.yaml draw -j ../config/eyelash_corne.json eyelash_corne_keymap.yaml > eyelash_corne_keymap.svg
+keymap -c ../keymap_drawer.config.yaml draw --keys-only --select-layers base fn num sys mouse  -j ../config/eyelash_corne.json eyelash_corne_keymap.yaml > eyelash_corne_keymap_keys.svg
+keymap -c ../keymap_drawer.config.yaml draw --combos-only --select-layers base fn num sys mouse -j ../config/eyelash_corne.json  eyelash_corne_keymap.yaml > eyelash_corne_keymap_combos.svg
